8955f43582efcd97390af898b7baf052677073c2
Marco Guadiana <marcoguadiana01@gmail.com>
2025-10-25 16:24:22 -0700

    Bookclass complete

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,17 @@
-class Author:
-    pass
-
-
 class Book:
-    pass
+    
+    all = []
 
+    def __init__(self, title: str):
+       
+        self.title = title
+        Book.all.append(self)
 
-class Contract:
-    pass
+    def contracts(self):
+        
+        return [contract for contract in Contract.all if contract.book is self]
+
+    def authors(self):
+       
+        return list(set([contract.author for contract in self.contracts()]))
+
