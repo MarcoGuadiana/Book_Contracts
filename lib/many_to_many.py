60c234b014b71f6642ec181b33f83347ffee24a2
Marco Guadiana <marcoguadiana01@gmail.com>
2025-10-25 16:26:16 -0700

    author class complete

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -15,3 +15,28 @@ class Book:
        
         return list(set([contract.author for contract in self.contracts()]))
 
+class Author:
+   
+    all = []
+
+    def __init__(self, name: str):
+        
+        self.name = name
+        Author.all.append(self)
+
+    def contracts(self):
+        
+        return [contract for contract in Contract.all if contract.author is self]
+
+    def books(self):
+        
+        return list(set([contract.book for contract in self.contracts()]))
+
+    def sign_contract(self, book: Book, date: str, royalties: int):
+       
+        return Contract(self, book, date, royalties)
+
+    def total_royalties(self):
+        
+        return sum(contract.royalties for contract in self.contracts())
+
